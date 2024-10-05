99613b8afdc438ea072e12fb159f9a7f48d711af
amStevee <stevenanongo2@gmail.com>
2024-10-05 07:48:18 -0700

    config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // images: {
+    //     remotePatterns: [
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'x.com',
+    //         port: '',
+    //         pathname: 'am_stevenanongo/photo/**',
+    //       },
+    //     ],
+    //   },
+};
 
 export default nextConfig;
